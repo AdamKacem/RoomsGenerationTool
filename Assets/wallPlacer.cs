ee2772ad24fe522e10bc7f7f8d2c1fa9ccb03e23
Adam Kacem <kacemadam9@gmail.com>
2025-07-17 13:43:31 +0100

    Fixed Wall Gaps

--- Assets/wallPlacer.cs
+++ Assets/wallPlacer.cs
@@ -7,9 +7,9 @@ public class wallPlacer : MonoBehaviour
     public GameObject CenterObject;
     public GameObject roomObject;
     public RoomGrid room;
-    
 
-    
+
+    public GameObject wall25, wall5, wall75;
 
    
 
@@ -34,41 +34,69 @@ public class wallPlacer : MonoBehaviour
 
         widthPos += origin;
         heightPos += origin;
-        Vector3 zTranslator = new Vector3(0,0, room.gridWidth + 2);
+        Vector3 zTranslator = new Vector3(0,0, room.gridHeight + 2);
         Vector3 xTranslator = new Vector3(room.gridWidth + 2,0, 0);
 
-        for (int i = 0; i < ((room.gridHeight+2)/4); i++)
+        for (int i = 0; i < ((room.gridWidth+2)/4); i++)
         {
             Instantiate(wall, widthPos, Quaternion.identity);
             Instantiate(wall, widthPos + zTranslator, rotation180);
             widthPos += new Vector3(4, 0, 0);
+    
+        }
+
+        int widthMod = (room.gridWidth + 2) % 4;
+        switch (widthMod)
+        {
+            case 1:
+                Instantiate(wall25, widthPos - new Vector3(1.5f,0,0), Quaternion.identity);
+                Instantiate(wall25, widthPos + zTranslator - new Vector3(1.5f, 0, 0), rotation180);
+                break;
+            case 2:
+                Instantiate(wall5, widthPos - new Vector3(1f, 0, 0), Quaternion.identity);
+                Instantiate(wall5, widthPos + zTranslator - new Vector3(1f, 0, 0) , rotation180);
+                break;
+            default:
+                Instantiate(wall75, widthPos - new Vector3(0.5f, 0, 0), Quaternion.identity);
+                Instantiate(wall75, widthPos + zTranslator - new Vector3(0.5f, 0, 0), rotation180);
+                break;
+        }
 
 
-            
 
-
-
-
-        } 
-        for (int i = 0; i < ((room.gridWidth+2)/4); i++)
+        for (int i = 0; i < ((room.gridHeight+2)/4); i++)
         {
             
             Instantiate(wall, heightPos, rotation90);
             Instantiate(wall, heightPos+xTranslator, rotationM90);
            
             heightPos += new Vector3(0, 0, 4);
-            
-            
+           
+        }
 
+        int heightMod = (room.gridHeight + 2) % 4;
+        switch (widthMod)
+        {
+            case 1:
+                Instantiate(wall25, heightPos - new Vector3(0, 0, 1.5f), rotation90);
+                Instantiate(wall25, heightPos + xTranslator - new Vector3(0, 0, 1.5f), rotationM90);
+                break;
+            case 2:
+                Instantiate(wall5, heightPos - new Vector3(0, 0, 1.5f), rotation90);
+                Instantiate(wall5, heightPos + xTranslator - new Vector3(0, 0, 1.5f), rotationM90);
+                break;
+            default:
+                Instantiate(wall75, heightPos - new Vector3(0, 0, 0.5f), rotation90);
+                Instantiate(wall75, heightPos + xTranslator - new Vector3(0, 0, 0.5f), rotationM90);
+                break;
         }
 
 
 
 
+        //GameObject height1 =  
+        //GameObject height2 =  Instantiate(wall, origin , rotation90);
 
-    //GameObject height1 =  
-    //GameObject height2 =  Instantiate(wall, origin , rotation90);
-    
 
 
 
